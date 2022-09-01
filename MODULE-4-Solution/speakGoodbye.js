a9724290a489385739b1cb82f0a93b231682a982
Suryansh Sharma <83308825+suryansh162@users.noreply.github.com>
2022-09-02 00:08:22 +0530

    Update speakGoodbye.js

--- MODULE-4-Solution/speakGoodbye.js
+++ MODULE-4-Solution/speakGoodbye.js
@@ -21,3 +21,14 @@
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
+
+
+
+
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+
+// STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// 'byeSpeaker' on the global scope as well.
+// xxxx.xxxx = byeSpeaker;
