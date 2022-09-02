e19595352b98f0cad94f9f342312650c8a28b7d2
Suryansh Sharma <83308825+suryansh162@users.noreply.github.com>
2022-09-02 10:57:17 +0530

    Update Script.js

--- MODULE-4-Solution/Script.js
+++ MODULE-4-Solution/Script.js
@@ -30,10 +30,10 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+let names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
+for (let i = 0; i < names.length; i++) {
+  let firstLetter = names[i].charAt(0).toLowerCase();
 
   if (firstLetter === 'j') {
     byeSpeaker(names[i]);
